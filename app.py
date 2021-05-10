ddcdff9abe471d3966004ec353134f407371f97b
cheon4050 <ehdcjs159@gmail.com>
2021-05-11 05:05:58 +0900

    todo app

--- app.py
+++ app.py
@@ -1,30 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-student_data = {
-    1: {"name": "슈퍼맨", "score": {"국어": 90, "수학": 65}},
-    2: {"name": "배트맨", "score": {"국어": 75, "영어": 80, "수학": 75}}
-}
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    content = db.Column(db.String(300), nullable=False)
 
+    def __rep__(self):
+        return '<Task %r>' %self.id
 
-@app.route('/')
-def index():
-    return render_template("index.html",
-                           template_students=student_data)
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error while adding the task'
+    else:
+        tasks = Todo.query.all()
+        return render_template("index.html", tasks=tasks)
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error while deketing the task'
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
 
-@app.route("/student/<int:id>")
-def student(id):
-    return render_template("student.html",
-                           template_name=student_data[id]["name"],
-                           template_score=student_data[id]["score"])
+    if request.method == 'POST':
+        task.content = request.form['content']
 
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue while updating that task'
+    else:
+        return render_template('update.html', task= task)
 
-@app.route('/user/<user_name>/<int:user_id>')
-def user(user_name, user_id):
-    return f'Hello, {user_name}({user_id})!'
-
-
-if __name__ == '__main__':
+if __name__=="__main__":
     app.run(debug=True)
+
+
